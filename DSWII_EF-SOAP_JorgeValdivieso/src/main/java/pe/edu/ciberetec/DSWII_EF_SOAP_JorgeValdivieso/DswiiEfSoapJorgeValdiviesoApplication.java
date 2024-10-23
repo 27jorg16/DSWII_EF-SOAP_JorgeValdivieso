3db29540058f39778e2264c9ba5f35f0bf55575a
27jorg16 <146002952+27jorg16@users.noreply.github.com>
2024-10-22 22:59:04 -0500

    Add files via upload

---
+++ DSWII_EF-SOAP_JorgeValdivieso/src/main/java/pe/edu/ciberetec/DSWII_EF_SOAP_JorgeValdivieso/DswiiEfSoapJorgeValdiviesoApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.ciberetec.DSWII_EF_SOAP_JorgeValdivieso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DswiiEfSoapJorgeValdiviesoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DswiiEfSoapJorgeValdiviesoApplication.class, args);
+	}
+
+}
---
+++ DSWII_EF-SOAP_JorgeValdivieso/src/main/java/pe/edu/ciberetec/DSWII_EF_SOAP_JorgeValdivieso/repository/CienteRepository.java
@@ -0,0 +1,4 @@
+package pe.edu.ciberetec.DSWII_EF_SOAP_JorgeValdivieso.repository;
+
+public interface CienteRepository {
+}
