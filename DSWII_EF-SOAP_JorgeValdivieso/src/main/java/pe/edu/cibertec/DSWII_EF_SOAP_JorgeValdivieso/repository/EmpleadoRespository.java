0006cf45ec14bf90af7c3520da7e1f76cbd3c756
27jorg16 <146002952+27jorg16@users.noreply.github.com>
2024-10-23 00:58:03 -0500

    Delete DSWII_EF-SOAP_JorgeValdivieso directory

--- DSWII_EF-SOAP_JorgeValdivieso/src/main/java/pe/edu/cibertec/DSWII_EF_SOAP_JorgeValdivieso/repository/EmpleadoRespository.java
+++
@@ -1,9 +0,0 @@
-package pe.edu.cibertec.DSWII_EF_SOAP_JorgeValdivieso.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import pe.edu.cibertec.DSWII_EF_SOAP_JorgeValdivieso.model.Empleado;
-
-@Repository
-public interface EmpleadoRespository extends JpaRepository<Empleado, Integer> {
-}
--- DSWII_EF-SOAP_JorgeValdivieso/src/main/java/pe/edu/cibertec/DSWII_EF_SOAP_JorgeValdivieso/repository/ProveedorRepository.java
+++
@@ -1,9 +0,0 @@
-package pe.edu.cibertec.DSWII_EF_SOAP_JorgeValdivieso.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import pe.edu.cibertec.DSWII_EF_SOAP_JorgeValdivieso.model.Proveedor;
-
-@Repository
-public interface ProveedorRepository extends JpaRepository<Proveedor, Integer> {
-}
